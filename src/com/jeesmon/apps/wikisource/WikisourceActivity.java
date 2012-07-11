b15fc0c8f61a7e1ecda564c5b99f20c0bfd2a69b
Jeesmon Jacob <jeesmon@gmail.com>
2012-07-11 16:36:03 -0400

    ML wikisource

---
+++ src/com/jeesmon/apps/wikisource/WikisourceActivity.java
@@ -0,0 +1,7 @@
+package com.jeesmon.apps.wikisource;
+
+import org.wikipedia.WikipediaActivity;
+
+public class WikisourceActivity extends WikipediaActivity {
+
+}
---
+++ src/com/jeesmon/apps/wikisource/WikisourceApp.java
@@ -0,0 +1,7 @@
+package com.jeesmon.apps.wikisource;
+
+import org.wikipedia.WikipediaApp;
+
+public class WikisourceApp extends WikipediaApp {
+
+}
